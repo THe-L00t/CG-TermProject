d7de0bd2ebd6ecf5a408b90b44ebdcc34996dbb0
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-11-12 16:54:19 +0900

    Initialize 함수 정의

--- Deadline_professor/Engine.cpp
+++ Deadline_professor/Engine.cpp
@@ -12,7 +12,19 @@ Engine::~Engine()
 void Engine::Initialize(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	w = std::make_unique<window>{ window() };
+	w = std::make_unique<window>();
+	w->Create();
+
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else {
+		std::cout << "GLEW Initialized\n";
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
 }
 
 void Engine::Run()
