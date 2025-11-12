942b3ff7774d108e65578ae7309baa436e7525c6
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-11-12 16:00:19 +0900

    Class window (#6)
    * 통합 헤더 사용
    
    * 아마 기초 베이스 완성?

---
+++ Deadline_professor/window.cpp
@@ -0,0 +1,34 @@
+#include "window.h"
+
+window::~window()
+{
+	Deactive();
+}
+
+bool window::Create()
+{
+	Active();
+
+	glutInitWindowSize(width, height);
+	glutCreateWindow(title.c_str());
+	return true;
+}
+
+void window::Active()
+{
+	activeInstance = this;
+}
+
+void window::Deactive()
+{
+	if (activeInstance == this) {
+		activeInstance = nullptr;
+	}
+}
+
+void window::Resize(int w, int h)
+{
+	width = w;
+	height = h;
+	glViewport(0, 0, w, h);
+}
