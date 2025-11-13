629a492da82448373ab8b6a100d81a6972318c77
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-11-13 17:00:55 +0900

    c++버전 수정 다른 브랜치에서도 자주 확인하기. 무조건 최신버전

--- Deadline_professor/Shader.cpp
+++ Deadline_professor/Shader.cpp
@@ -1,1 +1,19 @@
 ﻿#include "Shader.h"
+
+Shader::~Shader()
+{
+}
+
+Shader::Shader(Shader&& other) noexcept
+{
+}
+
+Shader& Shader::operator=(Shader&& other) noexcept
+{
+	// TODO: 여기에 return 문을 삽입합니다.
+}
+
+std::optional<std::string> Shader::LoadShader(const std::filesystem::path&)
+{
+	return std::optional<std::string>();
+}
