5b568cbd26f80828005723bb83a71bab70c2f424
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-11-13 16:47:33 +0900

    loadshader 함수 통합 헤더 갱신용 브랜치 만들기 전 저장

--- Deadline_professor/Shader.h
+++ Deadline_professor/Shader.h
@@ -3,6 +3,16 @@
 class Shader
 {
 public:
+	Shader() = default;
+	~Shader();
+
+	Shader(const Shader&) = delete;
+	Shader& operator=(const Shader&) = delete;
+
+	Shader(Shader&& other) noexcept;
+	Shader& operator=(Shader&& other) noexcept;
+
+	std::optional<std::string> LoadShader(const std::filesystem::path&);
 
 private:
 
