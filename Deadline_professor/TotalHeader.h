6ccb560229b4a5b4d27c8f1637588613c7789070
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-11-14 16:47:50 +0900

    헤더 추가 sstream과 filesystem

--- Deadline_professor/TotalHeader.h
+++ Deadline_professor/TotalHeader.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <functional>
 #include <optional>
+#include <sstream>
+#include <filesystem>
 #include <gl/glew.h> 
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
