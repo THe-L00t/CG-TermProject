0db5179991866f316fc06663ff38b13c638125f2
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-11-22 17:47:47 +0900

    콜백용 함수 구성

--- Deadline_professor/Renderer.cpp
+++ Deadline_professor/Renderer.cpp
@@ -4,7 +4,34 @@ Renderer::Renderer()
 
 }
 
+Renderer::~Renderer()
+{
+	Deactive();
+}
+
+void Renderer::Init()
+{
+	Active();
+}
+
+void Renderer::Active()
+{
+	activeInstance = this;
+}
+
+void Renderer::Deactive()
+{
+	if (activeInstance == this) {
+		activeInstance = nullptr;
+	}
+}
+
 void Renderer::OnWindowResize(int w, int h)
 {
 	glViewport(0, 0, w, h);
+}
+
+void Renderer::Display(GLvoid)
+{
+
 }
