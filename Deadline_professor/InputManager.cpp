031e3ee53cc39687b1ce967cd5e5168b20b5cd18
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-11-27 21:43:50 +0900

    통합헤더 병합 전 저장

---
+++ Deadline_professor/InputManager.cpp
@@ -0,0 +1,28 @@
+﻿#include "InputManager.h"
+
+InputManager* InputManager::onceInstance = nullptr;
+
+void InputManager::init()
+{
+	onceInstance = this;
+}
+
+void InputManager::SetMouseControlActive(bool active)
+{
+	mouseControl = active;
+	if (active) {
+		glutSetCursor(GLUT_CURSOR_NONE);
+	}
+}
+
+void InputManager::Keyboard(unsigned char, int, int)
+{
+}
+
+void InputManager::SKeyboard(int, int, int)
+{
+}
+
+void InputManager::Mouse(int, int, int, int)
+{
+}
---
+++ Deadline_professor/InputManager.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+class Camera
+class InputManager
+{
+public:
+	void init();
+
+	void SetMouseControlActive(bool);
+
+	//콜백용 함수 
+	static void Keyboard(unsigned char, int, int);
+	static void SKeyboard(int, int, int);
+	static void Mouse(int, int, int, int);
+private:
+	static InputManager* onceInstance;
+	Camera* c;
+	bool mouseControl{ false };
+};
+
