af16a4c86ed8965805bd0627dd4032c35336a608
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-12-04 12:25:33 +0900

    내용 업데이트

--- Deadline_professor/Object.cpp
+++ Deadline_professor/Object.cpp
@@ -1,1 +1,87 @@
 ﻿#include "Object.h"
+
+Object::Object()
+{
+}
+
+Object::~Object()
+{
+}
+
+void Object::Update(float deltaTime)
+{
+}
+
+void Object::UpdateModelMat()
+{
+	modelMat = glm::mat4(1.f);
+	modelMat = glm::translate(modelMat, position);
+	modelMat = glm::rotate(modelMat, glm::radians(rotation.x), glm::vec3(1.f, 0.f, 0.f));
+	modelMat = glm::rotate(modelMat, glm::radians(rotation.y), glm::vec3(0.f, 1.f, 0.f));
+	modelMat = glm::rotate(modelMat, glm::radians(rotation.z), glm::vec3(0.f, 0.f, 1.f));
+	modelMat = glm::scale(modelMat, scale);
+	needsUpdate = false;
+}
+
+void Object::SetPosition(const glm::vec3& pos)
+{
+	position = pos;
+	needsUpdate = true;
+}
+
+void Object::SetRotation(const glm::vec3& rot)
+{
+	rotation = rot;
+	needsUpdate = true;
+}
+
+void Object::SetScale(const glm::vec3& scl)
+{
+	scale = scl;
+	needsUpdate = true;
+}
+
+glm::vec3 Object::GetPosition() const
+{
+	return position;
+}
+
+glm::vec3 Object::GetRotation() const
+{
+	return rotation;
+}
+
+glm::vec3 Object::GetScale() const
+{
+	return scale;
+}
+
+glm::mat4 Object::GetModelMat()
+{
+	if (needsUpdate)
+	{
+		UpdateModelMat();
+	}
+	return modelMat;
+}
+
+void Object::SetResourceID(const std::string& id)
+{
+	resourceID = id;
+}
+
+std::string Object::GetResourceID() const
+{
+	return resourceID;
+}
+
+void Object::SetActive(bool active)
+{
+	isActive = active;
+}
+
+bool Object::IsActive() const
+{
+	return isActive;
+}
+
--- Deadline_professor/Object.h
+++ Deadline_professor/Object.h
@@ -1,5 +1,40 @@
 ﻿#pragma once
+#include "TotalHeader.h"
+
 class Object
 {
+public:
+	Object();
+	virtual ~Object();
+
+	virtual void Update(float);
+
+	void SetPosition(const glm::vec3&);
+	void SetRotation(const glm::vec3&);
+	void SetScale(const glm::vec3&);
+
+	glm::vec3 GetPosition() const;
+	glm::vec3 GetRotation() const;
+	glm::vec3 GetScale() const;
+	glm::mat4 GetModelMat();
+
+	void SetResourceID(const std::string&);
+	std::string GetResourceID() const;
+
+	void SetActive(bool);
+	bool IsActive() const;
+
+protected:
+	void UpdateModelMat();
+
+	glm::vec3 position{};
+	glm::vec3 rotation{};
+	glm::vec3 scale{1.f, 1.f, 1.f};
+	glm::mat4 modelMat{1.f};
+
+	std::string resourceID{};
+
+	bool isActive{true};
+	bool needsUpdate{true};
 };
 
