98d766284b71423137eccc4bcc94ef6933f79d73
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-12-04 15:14:58 +0900

    일단 완성

--- Deadline_professor/Professor.h
+++ Deadline_professor/Professor.h
@@ -1,5 +1,44 @@
-﻿#pragma once
-class Professor
-{
-};
+#pragma once
+#include "Object.h"
 
+class Professor : public Object
+{
+public:
+	Professor();
+	Professor(const std::string& meshKey, const std::string& animKey);
+	~Professor();
+
+	void Update(float deltaTime) override;
+
+	void SetMeshKey(const std::string& key);
+	void SetAnimationKey(const std::string& key);
+
+	std::string GetMeshKey() const;
+	std::string GetAnimationKey() const;
+
+	void SetPlayerPosition(const glm::vec3& playerPos);
+	void SetPlayerReference(Object* player);
+
+	void SetMoveSpeed(float speed);
+	float GetMoveSpeed() const;
+
+	void SetDetectionRange(float range);
+	float GetDetectionRange() const;
+
+	void SetDirection(const glm::vec3& dir);
+	glm::vec3 GetDirection() const;
+
+private:
+	void FleeFromPlayer(float deltaTime);
+
+	std::string meshKey{};
+	std::string animationKey{};
+
+	glm::vec3 playerPosition{};
+	glm::vec3 direction{};
+
+	Object* playerRef{nullptr};
+
+	float moveSpeed{5.0f};
+	float detectionRange{10.0f};
+};
