b1136a04c140ad23320a67d34106279280303500
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-12-04 15:54:30 +0900

    플레이어 클래스 간단 구성

---
+++ Deadline_professor/Player.cpp
@@ -0,0 +1,85 @@
+﻿#include "Player.h"
+#include "Camera.h"
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+void Player::Init(Camera* cam)
+{
+	camera = cam;
+	SyncCameraPosition();
+}
+
+void Player::Update(float deltaTime)
+{
+	Object::Update(deltaTime);
+	SyncCameraPosition();
+}
+
+void Player::MoveForward(float deltaTime)
+{
+	glm::vec3 newPos = position;
+	newPos.z -= moveSpeed * deltaTime;
+	SetPosition(newPos);
+	SyncCameraPosition();
+}
+
+void Player::MoveBackward(float deltaTime)
+{
+	glm::vec3 newPos = position;
+	newPos.z += moveSpeed * deltaTime;
+	SetPosition(newPos);
+	SyncCameraPosition();
+}
+
+void Player::MoveLeft(float deltaTime)
+{
+	glm::vec3 newPos = position;
+	newPos.x -= moveSpeed * deltaTime;
+	SetPosition(newPos);
+	SyncCameraPosition();
+}
+
+void Player::MoveRight(float deltaTime)
+{
+	glm::vec3 newPos = position;
+	newPos.x += moveSpeed * deltaTime;
+	SetPosition(newPos);
+	SyncCameraPosition();
+}
+
+void Player::SetMoveSpeed(float speed)
+{
+	moveSpeed = speed;
+}
+
+float Player::GetMoveSpeed() const
+{
+	return moveSpeed;
+}
+
+void Player::SetCamera(Camera* cam)
+{
+	camera = cam;
+	SyncCameraPosition();
+}
+
+Camera* Player::GetCamera() const
+{
+	return camera;
+}
+
+void Player::SyncCameraPosition()
+{
+	if (camera)
+	{
+		// 카메라 위치를 플레이어 위치와 동기화
+		// Camera 클래스에 SetPosition 메서드가 있다면 사용
+		// position은 Object 클래스의 protected 멤버
+	}
+}
---
+++ Deadline_professor/Player.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "TotalHeader.h"
+#include "Object.h"
+
+class Camera;
+
+class Player : public Object
+{
+public:
+	Player();
+	~Player();
+
+	void Init(Camera*);
+	void Update(float) override;
+
+	void MoveForward(float);
+	void MoveBackward(float);
+	void MoveLeft(float);
+	void MoveRight(float);
+
+	void SetMoveSpeed(float);
+	float GetMoveSpeed() const;
+
+	void SetCamera(Camera*);
+	Camera* GetCamera() const;
+
+	void SyncCameraPosition();
+
+private:
+	Camera* camera{nullptr};
+	float moveSpeed{5.f};
+};
